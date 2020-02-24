f09ed94f7df4d65e26822f733a166357fccfefa6
Nicolas Agostini <n.b.agostini@gmail.com>
2020-02-24 16:10:27 -0500

    Implement slug parsing and href

--- articles/models.py
+++ articles/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 # Create your models here.
 class Article (models.Model):
+
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
