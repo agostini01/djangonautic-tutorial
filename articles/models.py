2aa6fe4274357168ed9e9eef0c2f5290803535a0
Nicolas Agostini <n.b.agostini@gmail.com>
2020-02-24 13:39:34 -0500

    Include missing __init__.py files

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+# python manage.py makemigrations
+# python manage.py migrate
+
 # Create your models here.
 class Article (models.Model):
     title = models.CharField(max_length=100)
