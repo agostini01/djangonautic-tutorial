f11ab24138696dd05228c79b3b18d2b633486914
Nicolas Agostini <n.b.agostini@gmail.com>
2020-02-25 01:29:03 -0500

    Lecture 27: Implement saving new articles to DB

--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # python manage.py makemigrations
 # python manage.py migrate
 
 # Create your models here.
+
+
 class Article (models.Model):
 
     title = models.CharField(max_length=100)
@@ -11,6 +14,7 @@ class Article (models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
+    author = models.ForeignKey(User, default=None, on_delete=models.PROTECT)
 
     # add in thumbnail later
     # add in author later
@@ -18,9 +22,8 @@ class Article (models.Model):
     def __str__(self):
         return self.title
 
-
     def snippet(self):
-        if (len(self.body)>50):
+        if (len(self.body) > 50):
             return self.body[:50]+"..."
         else:
             return self.body
