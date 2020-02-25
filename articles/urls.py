bb33f5d59ad3a58ba1c72b642ba2efd4755768cb
Nicolas Agostini <n.b.agostini@gmail.com>
2020-02-24 23:50:45 -0500

    Implement user creation POST

--- articles/urls.py
+++ articles/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'articles'
 
 urlpatterns = [
-    path('', views.article_list,name='list'),
+    path('', views.article_list, name='list'),
     path('<slug:slug>/', views.article_detail, name='detail'),
 ]
-
