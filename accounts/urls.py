62dd1738742bd6addf36d973ff972c3da3e4f939
Nicolas Agostini <n.b.agostini@gmail.com>
2020-02-24 23:59:49 -0500

    Lecture 22: Implement login html and backend

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ app_name = 'accounts'
 
 
 urlpatterns = [
-    url('signup/', views.signup_view, name="signup")
+    url('signup/', views.signup_view, name="signup"),
+    url('login/', views.login_view, name="login"),
 ]
